1e1824f324efd8f8d1f703a6deda09c3294849c8
Evgeny Mamaev <evgeny.mamaev@leroymerlin.ru>
2019-07-28 16:51:42 +0300

    fill_generator_matrix, generator_matrix_from_a_matrix_transposed, transpose_matrix, multiply_matrices, compute_parity, negate_matrix, fill_i_matrix, append_matrix_to_front, and some small improvements

--- linearcode.py
+++ linearcode.py
@@ -1,5 +1,3 @@
-import array
-
 import math
 
 
@@ -21,75 +19,173 @@ class LinearCode(object):
         :param k: number of significant bits
         :param d: minimum code distance
         """
+        if not is_gilbert_varshamov_bound(n=n, k=k, d=d):
+            raise ValueError('The given n == {0}, k == {1} and d == {2} aren\'t compliant'
+                             ' with the Gilbert-Varshamov bound.'.format(n, k, d))
         self.n = n
         self.k = k
         self.d = d
         self.r = n - k
-        self.generator_matrix = array.array('L')  # use 32 bit unsigned integer
-        self.parity_check_matrix = [self.r]
-        self.a_matrix = []
-
-        self.i_matrix = []
-        # fill the unit matrix
-        for i in range(n - k):
-            self.i_matrix.append(1 << (n - k - i - 1))
-
-        if not is_gilbert_varshamov_bound(n=n, k=k, d=d):
-            raise ValueError('The given n == {0}, k == {1} and d == {2} aren\'t compliant'
-                             ' with the Gilbert-Varshamov bound.'.format(n, k, d))
+        self.parity_check_matrix, self.a_matrix_transposed = fill_parity_check_matrix_and_a_matrix_transposed(n=self.n,
+                                                                                                              r=self.r)
+        self.generator_matrix = fill_generator_matrix(n=self.n, k=self.k, r=self.r)
+        self.i_matrix = fill_i_matrix(size=self.r)
 
 
-def fill_parity_check_matrix(n, r):
+def fill_parity_check_matrix_and_a_matrix_transposed(n, r):
     """
-    Fills the 2-d array with the binary numbers from 1 to n,
-    column 0 contains binary 1 ...
-    column n - 1 contains binary n.
+    Produces H-matrix in standard form.
+    Fills the 2-d array with the binary
+    numbers from 1 to n so, that the
+    last columns contain the powers of
+    two in decreasing order.
 
     For example, for n = 7, r = 3:
-                0001111
-    binary:     0110011
-                1010101
+                0111100
+    binary:     1011010
+                1101001
 
-    decimal:    1234567
+    decimal:    3567421
 
     :param n: number of columns
     :param r: number of rows
     :return: 2-d array filled with binary numbers
     """
-    parity_check_matrix = [0] * r
+    k = n - r
+    a_matrix = [0] * r
+    powers_of_two, not_powers_of_two = partition_by_is_power_of_two(num=n)
+    powers_of_two.reverse()
+    not_powers_and_powers_of_two = not_powers_of_two + powers_of_two
+    a_matrix_transposed = []
+    for i in range(k):
+        num = not_powers_and_powers_of_two[i]
+        allocate_bits_in_column(
+            matrix=a_matrix,
+            position=i,
+            number=num,
+            n=n)
+        a_matrix_transposed.append(num)
+    return append_matrix_to_front(where=fill_i_matrix(r), what=a_matrix, lshift=0), a_matrix_transposed
+
+
+def fill_generator_matrix(n, k, r):
+    powers_of_two, not_powers_of_two = partition_by_is_power_of_two(num=n)
+    not_powers_of_two.reverse()
+    powers_and_not_powers_of_two = powers_of_two + not_powers_of_two
+    a_matrix = []
+    mask_of_r_length = (2 ** r - 1)
+    for i in range(k):
+        a_matrix.append(powers_and_not_powers_of_two[i] & mask_of_r_length)
+    return append_matrix_to_front(where=a_matrix, what=fill_i_matrix(k), lshift=r)
+
+
+def generator_matrix_from_a_matrix_transposed(transposed_a_matrix, k, r):
+    return append_matrix_to_front(
+        where=transposed_a_matrix,
+        what=fill_i_matrix(k),
+        lshift=r)
+
+
+def transpose_matrix(matrix, number_of_columns):
+    transposed_matrix = [0] * number_of_columns
+    k = len(matrix)
+    for i in range(k):
+        num = matrix[i]
+        allocate_bits_in_column(
+            matrix=transposed_matrix,
+            position=i,
+            number=num,
+            n=k)
+    return transposed_matrix
+
+
+def multiply_matrices(matrix1, num_col_m1, matrix2, num_col_m2):
+    m1_length = len(matrix1)
+    m2_length = len(matrix2)
+    if num_col_m1 != m2_length:
+        raise ValueError('The matrices are incompatible: '
+                         'matrix1 size is nxm = {0}x{1}, '
+                         'matrix2 size is mxp = {2}x{3}. '
+                         'm != m '.format(m1_length, num_col_m1, m2_length, num_col_m2))
+    transposed_matrix2 = transpose_matrix(matrix2, num_col_m2)
+    result = [0] * m1_length
+    for i in range(m1_length):
+        for j in range(num_col_m2):
+            result[i] = result[i] | compute_parity(matrix1[i] & transposed_matrix2[j], m2_length) << (
+                        num_col_m2 - j - 1)
+    return result
+
+
+def compute_parity(number, length):
+    """
+    Computes the parity, which is a number of 1s in
+    the binary representation of a given number.
+    Can be used to calculate the sum of all 1s.
+    :param number: any number to compute the parity.
+    :param length: the length of the number,
+    specified explicitly.
+    :return: 1 if the number of 1s is odd,
+    0 if the number of 1s is even.
+    """
+    number = number & (2 ** length - 1)
+    i = 1
+    while i <= length:
+        number = number ^ (number >> i)
+        i *= 2
+    return number & 1
+
+
+def negate_matrix(matrix, r):
+    k = len(matrix)
+    negated_matrix = [0] * k
+    for i in range(k):
+        negated_matrix[i] = matrix[i] ^ (2 ** r - 1)
+    return negated_matrix
+
+
+def fill_i_matrix(size):
+    i_matrix = []
+    for i in range(size):
+        i_matrix.append(1 << (size - i - 1))
+    return i_matrix
+
+
+def append_matrix_to_front(where, what, lshift):
+    if len(where) != len(what):
+        raise ValueError('The given matrices are different in their sizes:'
+                         'matrix1.size() == {0}, matrix2.size() == {1}'
+                         .format(len(where), len(what)))
+    for i in range(len(where)):
+        where[i] = where[i] | what[i] << lshift
+    return where
+
+
+def partition_by_is_power_of_two(num):
     powers_of_two = []
     not_powers_of_two = []
-    for i in range(n):
+    for i in range(num):
         if is_power_of_two(num=i + 1):
             powers_of_two.append(i + 1)
         else:
             not_powers_of_two.append(i + 1)
-    i = 0
-    for num in not_powers_of_two:
-        allocate_bits_in_column(parity_check_matrix=parity_check_matrix, position=i, number=num, n=n)
-        i += 1
-    powers_of_two.reverse()
-    for num in powers_of_two:
-        allocate_bits_in_column(parity_check_matrix=parity_check_matrix, position=i, number=num, n=n)
-        i += 1
-    return parity_check_matrix
+    return powers_of_two, not_powers_of_two
 
 
-def allocate_bits_in_column(parity_check_matrix, position, number, n):
+def allocate_bits_in_column(matrix, position, number, n):
     """
 
     Not a pure function, which modifies the passed 2-d array by filling
     any given row with the binary representation of the given number
 
-    :param parity_check_matrix: the matrix is to be filled
+    :param matrix: the matrix is to be filled
     :param position: position of a bit in a binary row, from left to right
     :param number: a number is to be split and allocated in a column
            with the aforementioned position
     :param n: number of columns
     """
-    r = len(parity_check_matrix)
+    r = len(matrix)
     for j in range(r):
-        parity_check_matrix[j] = parity_check_matrix[j] | (
+        matrix[j] = matrix[j] | (
             (1 << (n - 1 - position)) if number & (1 << (r - 1 - j)) else 0)
 
 
--- linearcodetest.py
+++ linearcodetest.py
@@ -2,26 +2,31 @@ import unittest
 
 from linearcode import LinearCode
 from linearcode import allocate_bits_in_column
-from linearcode import fill_parity_check_matrix
+from linearcode import append_matrix_to_front
+from linearcode import compute_parity
+from linearcode import fill_generator_matrix
+from linearcode import fill_i_matrix
+from linearcode import fill_parity_check_matrix_and_a_matrix_transposed
+from linearcode import generator_matrix_from_a_matrix_transposed
 from linearcode import is_gilbert_varshamov_bound
 from linearcode import is_power_of_two
+from linearcode import multiply_matrices
+from linearcode import negate_matrix
+from linearcode import partition_by_is_power_of_two
+from linearcode import transpose_matrix
 
-
+# TODO test exceptions
 class LinearCodeTest(unittest.TestCase):
     def test_init(self):
-        linear_code = LinearCode(10, 5, 4)
+        linear_code = LinearCode(n=10, k=3, d=3)
         print(bin(linear_code.i_matrix[1]))
-        assert True
         pass
 
-    def test_gilbert_varshamov_false(self):
+    def test_is_gilbert_varshamov_bound(self):
         #       /10 - 1\     /10 - 1\             5
         # 1  +  |      |  +  |      |  =  46  >  2  =  32  => false
         #       \   1  /     \   2  /
         assert not is_gilbert_varshamov_bound(10, 5, 4)
-        pass
-
-    def test_gilbert_varshamov_true(self):
         #       /12 - 1\     /12 - 1\             8
         # 1  +  |      |  +  |      |  =  67  <  2  =  256  => true
         #       \   1  /     \   2  /
@@ -32,14 +37,14 @@ class LinearCodeTest(unittest.TestCase):
         matrix = [0, 0, 0]
         n = 10
         position = 1
-        allocate_bits_in_column(parity_check_matrix=matrix, position=position, number=2, n=n)
+        allocate_bits_in_column(matrix=matrix, position=position, number=2, n=n)
         assert 2 ** (n - 1 - position) == matrix[1]
         pass
 
-    def test_fill_parity_check_matrix(self):
+    def test_fill_parity_check_matrix_and_a_matrix_transposed(self):
         n = 10
-        r = 3
-        parity_check_matrix = fill_parity_check_matrix(n, r)
+        r = 7
+        parity_check_matrix, a_matrix_transposed = fill_parity_check_matrix_and_a_matrix_transposed(n, r)
         print()
         for line in parity_check_matrix:
             print(format(line, '#0{0}b'.format(n + 2)))
@@ -50,6 +55,119 @@ class LinearCodeTest(unittest.TestCase):
         assert is_power_of_two(num)
         num = 255
         assert not is_power_of_two(num)
+        pass
+
+    def test_fill_i_matrix(self):
+        r = 10
+        matrix = fill_i_matrix(size=r)
+        i = 0
+        for num in matrix:
+            assert 2 ** (r - 1 - i) == num
+            i += 1
+        pass
+
+    def test_append_matrix_to_front(self):
+        n = 10
+        k = 7
+        r = n - k
+        matrix1 = []
+        print()
+        for i in range(k):
+            matrix1.append(i + 1)
+        matrix2 = fill_i_matrix(k)
+        i = 0
+        for num in append_matrix_to_front(matrix1, matrix2, r):
+            # each number consists of the power of two and an element of an I-matrix
+            assert num == 2 ** (n - 1 - i) + (i + 1)
+            i += 1
+        pass
+
+    def test_fill_generator_matrix(self):
+        n = 10
+        k = 7
+        r = n - k
+        print()
+        for i in fill_generator_matrix(n, k, r):
+            print(format(i, '#0{0}b'.format(n + 2)))
+        pass
+
+    def test_negate_matrix(self):
+        n = 5
+        print()
+        for i in negate_matrix(fill_i_matrix(n), 10):
+            print(format(i, '#0{0}b'.format(n + 2)))
+        pass
+
+    def test_generator_matrix_from_a_matrix_transposed(self):
+        n = 10
+        k = 3
+        r = n - k
+        parity_check_matrix, a_matrix_transposed = fill_parity_check_matrix_and_a_matrix_transposed(n, r)
+        print()
+        for i in parity_check_matrix:
+            print(format(i, '#0{0}b'.format(n + 2)))
+        print()
+        for i in generator_matrix_from_a_matrix_transposed(a_matrix_transposed, k, r):
+            print(format(i, '#0{0}b'.format(n + 2)))
+
+    def test_transpose_matrix(self):
+        n = 10
+        r = 7
+        matrix, a_matrix_transposed = fill_parity_check_matrix_and_a_matrix_transposed(n, r)
+        print()
+        for i in matrix:
+            print(format(i, '#0{0}b'.format(n + 2)))
+        print()
+        transposed_matrix = transpose_matrix(matrix, n)
+        for i in transposed_matrix:
+            print(format(i, '#0{0}b'.format(r + 2)))
+        print()
+        doubly_transposed_matrix = transpose_matrix(transposed_matrix, r)
+
+        for i in range(len(doubly_transposed_matrix)):
+            assert doubly_transposed_matrix[i] == matrix[i]
+            print(format(doubly_transposed_matrix[i], '#0{0}b'.format(n + 2)))
+
+    def test_compute_parity(self):
+        number_parity_0 = 0b101
+        number_parity_exceeding_length_0 = 0b1101
+        number_parity_1 = 0b11111111111
+        number_parity_exceeding_length_1 = 0b111111111111
+        assert compute_parity(number_parity_0, 3) == 0
+        assert compute_parity(number_parity_exceeding_length_0, 3) == 0
+        assert compute_parity(number_parity_1, 11) == 1
+        assert compute_parity(number_parity_exceeding_length_1, 3) == 1
+        pass
+
+    def test_multiply_matrices(self):
+        n = 10
+        k = 3
+        r = n - k
+        parity_check_matrix, a_matrix_transposed = fill_parity_check_matrix_and_a_matrix_transposed(n, r)
+        generator_matrix = generator_matrix_from_a_matrix_transposed(a_matrix_transposed, k, r)
+        multiplied_matrix = multiply_matrices(parity_check_matrix, n, transpose_matrix(generator_matrix, n), k)
+        print()
+        for i in multiplied_matrix:
+            assert i == 0
+        pass
+
+    def test_partition_by_is_power_of_two(self):
+        n = 100
+        powers_of_two, not_powers_of_two = partition_by_is_power_of_two(n)
+        max_value = 0
+        for i in range(n):
+            max_value += i + 1
+        i = 1
+        counter = 0
+        while True:
+            if i <= n:
+                counter += 1
+                i *= 2
+            else:
+                break
+        assert len(powers_of_two) == counter
+        assert len(not_powers_of_two) == n - counter
+        assert sum(powers_of_two) + sum(not_powers_of_two) == max_value
         pass
 
 
